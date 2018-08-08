fee771ce9c9e84035699029605d6965112323a1d
Seonyu Kim <>
2018-08-09 01:49:36 +0900

    edit file path

--- file/bee01.js
+++ file/bee01.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   window.sr = ScrollReveal();
   sr.reveal('.reveal');
-
-
 });
+
+
+var num = 1;
+
+function gallery(num) {
+  var imgTag = document.getElementById("photo");
+  imgTag.setAttribute("src", "../imgs/bee_img_" + num + ".png");
+}
+
+
+
