425f42af04579a1d6eff4e70da27efc1328e2364
Seonyu Kim <>
2018-08-10 01:42:15 +0900

    edit main js

--- file/about.js
+++ file/about.js
@@ -6,11 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 let $sun = $(".sun");
+let $sun02 = $(".sun-02");
 
 let sunRise = function () {
-    TweenMax.fromTo($sun, 3, { autoAlpha: 0 }, { autoAlpha: 1 });
+    TweenMax.fromTo($sun, 1, { autoAlpha: 0 }, { autoAlpha: 1 });
     new TweenMax($sun, 2, { y: -140 });
-    new TweenMax($sun, 2, {}).delay(2);
+    new TweenMax($sun, 2, {
+        ease: Elastic.easeOut.config(1, 0.7)
+    }).delay(4);
+    TweenMax.fromTo($sun02, 10, { autoAlpha: 0 }, { autoAlpha: 0.6 });
 }
 
 sunRise();
@@ -23,8 +27,10 @@ let sunAnimation = function () {
             scale: 1.2,
             opacity: 1,
             ease: Elastic.easeOut.config(1, 0.7)
-        });
+        }
+        );
     });
+
 
     $sun.on("mouseleave", function () {
         TweenMax.to($(this), 0.6, {
