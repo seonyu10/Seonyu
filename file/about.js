1185dfab9b4f5d1b3f41d822f9a699fa0f5f1d63
Seonyu Kim <>
2018-08-13 11:26:52 +0900

    add easteregg

--- file/about.js
+++ file/about.js
@@ -1,31 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
-    window.sr = ScrollReveal({ reset: true });
-    sr.reveal('.intro');
 
-});
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+//----------------------//
 
 let $sun = $(".sun");
 let $sun02 = $(".sun-02");
 
 let sunRise = function () {
-    TweenMax.fromTo($sun, 1, { autoAlpha: 0 }, { autoAlpha: 1 });
-    new TweenMax($sun, 2, { y: -140 });
-    new TweenMax($sun, 2, {
-        ease: Elastic.easeOut.config(1, 0.7)
-    }).delay(4);
-    TweenMax.fromTo($sun02, 10, { autoAlpha: 0 }, { autoAlpha: 0.6 });
+    TweenMax.fromTo($sun, 3,
+        { css: { bottom: "-100" } },
+        {
+            css: {
+                bottom: "-32",
+                ease: Elastic.easeOut.config(1, 0.7)
+            }
+        });
 }
 
 sunRise();
 
+let sunRise02 = function () {
+    TweenMax.fromTo($sun02, 2,
+        { autoAlpha: 0 }, { autoAlpha: 0.4, delay: 2 });
+}
+sunRise02();
 
+
+//---------------sunAnimation
 
 let sunAnimation = function () {
     $sun.on("mouseenter", function () {
-        TweenMax.to($(this), 0.6, {
+        TweenMax.to($(this), 1, {
             scale: 1.2,
-            opacity: 1,
             ease: Elastic.easeOut.config(1, 0.7)
         }
         );
@@ -33,10 +65,8 @@ let sunAnimation = function () {
 
 
     $sun.on("mouseleave", function () {
-        TweenMax.to($(this), 0.6, {
+        TweenMax.to($(this), 1, {
             scale: 1,
-            opacity: 0.8,
-            ease: Power2.easeOut
         });
     });
 };
