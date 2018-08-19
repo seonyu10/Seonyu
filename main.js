d90e182d74f27dac3ca720dfa4957bfb8bf39ee1
Seonyu Kim <seonyukim@Seonyu.local>
2018-08-19 13:00:46 +0900

    index numbering 추가

--- main.js
+++ main.js
@@ -4,39 +4,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-let attachEvent = function () {
+let hoverEvent = function () {
   $(".card").on("mouseenter", function () {
     TweenMax.to($(this), 0.6, {
       scale: 1.05,
-      opacity: 1,
       ease: Elastic.easeOut.config(1, 0.7)
     });
+
+
   });
 
   $(".card").on("mouseleave", function () {
     TweenMax.to($(this), 0.6, {
       scale: 1,
-      opacity: 1,
       ease: Power2.easeOut
     });
   });
 };
 
-$.ajax({
-  type: "GET",
-  dataType: "jsonp",
-  url:
-    "https://api.behance.net/v2/users/seonyu10/projects?client_id=ADC7ff9sfVnDp8uUxdQdK50eLgYyxSZp",
-  success: function (data) {
-    attachEvent(data);
-  },
-  error: function (error) {
-    console.log(error);
-  }
-});
-
-
-
+hoverEvent();
 
 
 
